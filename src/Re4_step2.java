b204f2f6672a24c0ce5b1de4e57465e00b48d9c8
yuki yosigai <e175760@ie.u-ryukyu.ac.jp>
2017-11-28 08:06:51 +0900

    first_1

--- src/Re4_step2.java
+++ src/Re4_step2.java
@@ -1,2 +1,14 @@
+
+import java.lang.NullPointerException;
+
 public class Re4_step2 {
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("間違えています");
+
+        }
+    }
 }
