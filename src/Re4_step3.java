9e2d358d31d20418030a731809684b8bebb56fbb
yuki yosigai <e175760@ie.u-ryukyu.ac.jp>
2017-11-28 08:26:52 +0900

    first_1

---
+++ src/Re4_step3.java
@@ -0,0 +1,7 @@
+public class Re4_step3 {
+    public static void main(String[] args){
+        String str = "3．14";
+        double value = Double.parseDouble(str);
+    System.out.println(value);
+    }
+}
